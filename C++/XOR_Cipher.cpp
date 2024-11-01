b6c27667a4607746b87eb71b4f12048f34777be5
ThanhNguyenX <112296523+thannhf@users.noreply.github.com>
2024-11-01 11:36:37 +0700

    Update XOR_Cipher.cpp

--- C++/XOR_Cipher.cpp
+++ C++/XOR_Cipher.cpp
@@ -1,9 +1,14 @@
+// đây là một chương trình mã hóa và giải mã chuỗi văn bản sử dụng thuật toán XOR
+//đoạn mã này gồm hai hàm encrypt và decrypt nằm trong namespace(không gian tên) sử dụng các phép toán XOR giữa các ký tự trong chuỗi đầu vào và một khóa (key) để mã hóa và giải mã văn bản
 #include <iostream>
 #include <string>
 #include <cassert>
-
+//vì phép toán XOR này chỉ đơn giản là phép toán giữa mỗi ký tự và key nên phương pháp này rất dễ cài đặt nhanh chóng
+//đi kèm với sự tiện lợi về cài đặt đó là sự không an toàn cho các ứng dụng yêu cầu bảo mật cao, vì một khóa ngắn và đơn giản có thể dễ dàng bị bẻ khóa.
 namespace ciphers{
 	namespace XOR{
+		//hàm này lặp qua duyệt từng ký tự c trong chuỗi text thực hiện phép toán XOR giữa ký tự c và key sau đó chuyển về kiểu char
+		//sau khi đã mã hóa thì thêm ký tự đã mã hóa vào encrypted_text và trả về chuỗi đã mã hóa.
 		std::string encrypt (const std::string &text, const int &key) {
 			std::string encrypted_text = "";
 			for(auto &c : text) {
@@ -12,6 +17,9 @@ namespace ciphers{
 			}
 			return encrypted_text;
 		}
+		//hàm decrypt thực hiện các bước tương tự như mã hóa nhưng thay vì mã hóa nó dùng XOR để giải mã vì phép toán XOR nghịch đảo chính nó sử dụng cùng một khóa key để XOR nó lại một lần nữa sẽ trả lại bản rõ
+		//vòng lặp duyệt qua từng ký tự c trong chuỗi mã hóa text thực hiện XOR giữa ký tự đã mã hóa và key, lấy lại ký tự ban đầu
+		//thêm ký tự đã giải mã vào decrypted_text và trả về chuỗi bản rõ.
 		std::string decrypt (const std::string &text, const int &key) {
 			std::string decrypted_text = "";
 			for(auto &c : text) {
@@ -22,7 +30,7 @@ namespace ciphers{
 		}
 	}
 }
-
+//test thuật toán
 void test() {
 	//test 1
 	std::string text1 = "Whipalsh! : Do watch this movie...";

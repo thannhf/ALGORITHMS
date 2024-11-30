cb7aa6e52dcd8a7e9d8fa7849bff8bb48fa1042c
ThanhNguyenX <112296523+thannhf@users.noreply.github.com>
2024-11-30 11:00:45 +0000

    upload

---
+++ leetcode_and_hackerrank/leetcode/easy/145. Binary Tree Postorder Traversal.js
@@ -0,0 +1,41 @@
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+// Example 1:
+// Input: root = [1,null,2,3]
+// Output: [3,2,1]
+
+// Example 2:
+// Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+// Output: [4,6,7,5,2,9,8,3,1]
+
+// Example 3:
+// Input: root = []
+// Output: []
+
+// Example 4:
+// Input: root = [1]
+// Output: [1]
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    let res = [];
+    traverse = (node) => {
+        if(!node) return;
+        traverse(node.left);
+        traverse(node.right);
+        res.push(node.val);
+    }
+    traverse(root);
+    return res;
+};

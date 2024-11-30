a3b15d43aae0b546761d438d8f8de2da39325e7a
ThanhNguyenX <112296523+thannhf@users.noreply.github.com>
2024-11-30 08:58:54 +0000

    upload

---
+++ leetcode_and_hackerrank/leetcode/easy/101. Symmetric Tree.js
@@ -0,0 +1,34 @@
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+// Example 1:
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+
+// Example 2:
+// Input: root = [1,2,2,null,3,null,3]
+// Output: false
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(!root)
+        return true;
+    return isSame(root.left, root.right);
+};
+var isSame = function(leftroot, rightroot) {
+    if((!leftroot && rightroot) || (leftroot && !rightroot) || (leftroot && rightroot && leftroot.val !== rightroot.val))
+        return false;
+    if(leftroot && rightroot)
+        return isSame(leftroot.left, rightroot.right) && isSame(leftroot.right, rightroot.left);
+    return true;
+}

1d2315359daa378e8e8670378df03248da1675a7
ThanhNguyenX <112296523+thannhf@users.noreply.github.com>
2024-12-04 16:27:23 +0000

    upload

---
+++ leetcode_and_hackerrank/leetcode/easy/283. Move Zeroes.js
@@ -0,0 +1,35 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let low = 0;
+    let high = low + 1;
+    while(high <= nums.length - 1) {
+        if(nums[low] !== 0) {
+            low++;
+            high++;
+        } else {
+            if(nums[high] !== 0) {
+                [nums[low], nums[high]] = [nums[high], nums[low]];
+                low++;
+            }
+            high++;
+        }
+    } 
+};

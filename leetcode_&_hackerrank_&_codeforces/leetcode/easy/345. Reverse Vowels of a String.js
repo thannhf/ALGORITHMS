44ed00e0b307fe48214c1a8625669abe1de0abba
ThanhNguyenX <112296523+thannhf@users.noreply.github.com>
2024-12-05 16:02:50 +0000

    upload

---
+++ leetcode_&_hackerrank_&_codeforces/leetcode/easy/345. Reverse Vowels of a String.js
@@ -0,0 +1,30 @@
+// Given a string s, reverse only all the vowels in the string and return it.
+
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+// Example 1:
+
+// Input: s = "IceCreAm"
+
+// Output: "AceCreIm"
+
+// Explanation:
+
+// The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+// Example 2:
+
+// Input: s = "leetcode"
+
+// Output: "leotcede"
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    const vow = s.match(/[aeiou]/gi);
+    return s.replace(/[aeiou]/ig, el => vow.pop());
+};

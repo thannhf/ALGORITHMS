ce290af82f5a7f8996e1b2878061c3a73d14ba8d
ThanhNguyenX <112296523+thannhf@users.noreply.github.com>
2024-12-07 04:04:04 +0000

    upload

---
+++ leetcode_&_hackerrank_&_codeforces/leetcode/easy/383. Ransom Note.js
@@ -0,0 +1,31 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+// Example 1:
+
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+// Example 2:
+
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    for(const char of magazine) {
+        ransomNote = ransomNote.replace(char, "");
+    }
+    if(!ransomNote) return true;
+    else return false;
+};

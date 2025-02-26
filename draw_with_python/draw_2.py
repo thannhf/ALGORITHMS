57c1f56797fe625017df1b240e93aaeb3efaa673
ThanhNguyenX <112296523+thannhf@users.noreply.github.com>
2025-02-26 16:32:00 +0700

    Update draw_2.py

--- draw_with_python/draw_2.py
+++ draw_with_python/draw_2.py
@@ -1,3 +1,10 @@
+# pattern = n = 5
+# k = 2 * n - 2 => k = 2 * 5 - 2 = 8 with a line  - 1 white spaces => top triangle
+# for 1 - i: 0 -> n => 1
+# for 1 - j: 0 -> i + 1 => 0 -> 1 + 1 = 2.
+##############################
+# with bottom triangle we have reverse top triangle
+##########################################
 def pattern(n):
     k = 2 * n - 2
     for i in range(0, n):
@@ -16,10 +23,3 @@ def pattern(n):
             print("* ", end="")
         print("\r")
 pattern(5)
-
-# pattern = n = 5
-# k = 2 * n - 2 => k = 2 * 5 - 2 = 8 with a line  - 1 white spaces => top triangle
-# for 1 - i: 0 -> n => 1
-# for 1 - j: 0 -> i + 1 => 0 -> 1 + 1 = 2.
-#############################3
-# with bottom triangle we have reverse top triangle

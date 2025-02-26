94041f199745faf196ef6ec2eacc2ca87f45308d
ThanhNguyenX <112296523+thannhf@users.noreply.github.com>
2025-02-26 09:22:53 +0000

    update

--- draw_with_python/draw_1.py
+++ draw_with_python/draw_1.py
@@ -4,3 +4,8 @@ def pattern(n):
             print("* ", end="")
         print("\n")
 pattern(5)
+
+# i = 5
+# for 1 - i: 0 -> n, => 1
+# for 1 - j: 0 -> i + 1 => 1 + 1 = 2
+# print is space between lines for
